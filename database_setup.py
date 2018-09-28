9fcb2d60eb2c08d2093eb1678bcb81f2c94af2cd
Laurens Deprost <ldeprost@gmail.com>
2018-09-29 00:46:01 +0200

    Added price to catalog json

--- database_setup.py
+++ database_setup.py
@@ -48,10 +48,11 @@ class Item(Base):
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
+            'id': self.id,
             'name': self.name,
             'description': self.description,
-            'id': self.id,
-            'user_id': self.user_id,
+            'price': '$' + self.price,
+            'user_id': self.user_id
         }
 
 
