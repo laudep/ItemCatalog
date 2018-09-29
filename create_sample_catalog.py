28b1f5e081049fdb7229fc73482ace372fb01641
Laurens Deprost <ldeprost@gmail.com>
2018-09-29 13:39:16 +0200

    Added drinks to sample data generator

--- create_sample_catalog.py
+++ create_sample_catalog.py
@@ -9,12 +9,13 @@ import warnings
 from database_setup import *
 from application import session
 
+
 def createCategory(name, user_id):
     """ Create an item category """
     c = Category(name=name, user_id=user_id)
     session.add(c)
     session.commit()
-    print "Category", name, "created."
+    print 'Category "' + name + '" created.'
     return c
 
 
@@ -25,10 +26,11 @@ def createItem(name, category, price, user_id):
     except wikipedia.exceptions.DisambiguationError as e:
         description = wikipedia.summary(name + " " + category.name)
 
-    i = Item(name=name, description=description, category_id=category.id, price=price, user_id=user_id)
+    i = Item(name=name, description=description,
+             category_id=category.id, price=price, user_id=user_id)
     session.add(i)
     session.commit()
-    print "Item", name, "added."
+    print 'Item "' + name + '" added.'
     return i
 
 
@@ -37,7 +39,7 @@ def createUser(name, email, picture):
     u = User(name=name, email=email, picture=picture)
     session.add(u)
     session.commit()
-    print "User", name, "created."
+    print 'User "' + name + '" created.'
     return u
 
 
@@ -71,7 +73,7 @@ def install_and_import(package):
         globals()[package] = importlib.import_module(package)
 
 
-# install wikipedia using PIP if necessary
+# install wikipedia using PIP if necessary and import the module
 install_and_import('wikipedia')
 
 # Clear database tables
@@ -79,16 +81,17 @@ clearDatabase()
 
 # Create sample data
 print "Generating sample data..."
+print
 sampleUser = createUser('Sample Data', 'john@smith.com',
                         'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png')
-
-fruit = createCategory('Fruit', sampleUser.id)
-vegetables = createCategory('Vegetables', sampleUser.id)
 
 # supress BeautifulSoup warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
+
+# fruit
+fruit = createCategory('Fruit', sampleUser.id)
 createItem('Apple',
            fruit,
            '0.49',
@@ -114,6 +117,9 @@ createItem('Strawberries',
            '2.22',
            sampleUser.id)
 
+# vegetables
+print
+vegetables = createCategory('Vegetables', sampleUser.id)
 createItem('Broccoli',
            vegetables,
            '1.99',
@@ -139,5 +145,33 @@ createItem('Tomato',
            '3',
            sampleUser.id)
 
+# drinks
+print
+drinks = createCategory('Drinks', sampleUser.id)
+createItem('Beer',
+           drinks,
+           '2.50',
+           sampleUser.id)
 
+createItem('Coca Cola',
+           drinks,
+           '1',
+           sampleUser.id)
+
+createItem('Mineral water',
+           drinks,
+           '1.50',
+           sampleUser.id)
+
+createItem('Orange juice',
+           drinks,
+           '2',
+           sampleUser.id)
+
+createItem('Sparkling Water',
+           drinks,
+           '1.50',
+           sampleUser.id)
+
+print
 print "Database refreshed and filled with sample data."
